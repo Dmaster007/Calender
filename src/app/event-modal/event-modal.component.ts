be93813b18045150e44784d6129a5c1166d5aa95
Durgesh Agrawal <102041586+Dmaster007@users.noreply.github.com>
2024-06-11 16:07:13 +0530

    updated some features

--- src/app/event-modal/event-modal.component.ts
+++ src/app/event-modal/event-modal.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
+import { Component, EventEmitter, Inject, Output } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogContent, MatDialogModule } from '@angular/material/dialog';
+import Event from '../event';
+import { CommonModule, DatePipe, UpperCasePipe  ,TitleCasePipe} from '@angular/common';
+import { EventsService } from '../events.service';
+
 
 @Component({
   selector: 'app-event-modal',
   standalone: true,
-  imports: [MatDialogModule],
+  imports: [MatDialogModule,MatDialogContent,UpperCasePipe, DatePipe,CommonModule],
   templateUrl: './event-modal.component.html',
   styleUrl: './event-modal.component.css'
 })
+
+
+
 export class EventModalComponent {
+
+    constructor(@Inject(MAT_DIALOG_DATA) public data: Event , public events : EventsService){
+    }
+    // @Output() deleteevent = new EventEmitter<string>();
+    deleteEvent(){
+      this.events.events = this.events.events.filter((event)=> event.id !== this.data.id)
+      // this.deleteevent.emit('deleted');
+    }
 
 }
--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,18 +1,31 @@
 import { CommonModule, DatePipe } from '@angular/common';
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  SimpleChanges,
+} from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { EventsService } from '../events.service';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { EventModalComponent } from '../event-modal/event-modal.component';
 import Event from '../event';
+
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [DatePipe, CommonModule, ReactiveFormsModule,MatDialogModule],
+  imports: [
+    DatePipe,
+    CommonModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    EventModalComponent,
+  ],
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.css']
+  styleUrls: ['./events.component.css'],
 })
-export class EventsComponent implements OnChanges {
+export class EventsComponent implements OnChanges, OnDestroy {
   @Input() selected: Date | null = null;
 
   title = new FormControl<string | null>('');
@@ -20,29 +33,37 @@ export class EventsComponent implements OnChanges {
 
   newEvent = new FormGroup({
     title: this.title,
-    desc: this.desc
+    desc: this.desc,
   });
-
+  color = 'bg-indigo-100';
+  selectedColor: string | null = 'indigo';
   currEvents: Event[] = [];
+  
+  constructor(public events: EventsService, public dialog: MatDialog) {}
 
-  constructor(public events: EventsService , public dialog: MatDialog) {}
-
-  openDialog(event : Event ) {
-    const dialogRef = this.dialog.open(EventModalComponent ,{
-      data : {
-        title : event.title,
-        date :event.date,
-        id : event.id,
-        desc : event.date
-
-      }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
+  selectColor(color: string): void {
+    this.selectedColor = color;
+    this.color = `bg-${color}-100`;
+    // console.log(this.color);
   }
 
+  openDialog(event: Event) {
+    const dialogRef = this.dialog.open(EventModalComponent, {
+      data: {
+        title: event.title,
+        date: event.date,
+        id: event.id,
+        desc: event.desc,
+        color: event.color,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.updateCurrEvents();
+      }
+    });
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['selected'] && changes['selected'].currentValue) {
@@ -50,30 +71,52 @@ export class EventsComponent implements OnChanges {
     }
   }
 
+  ngOnDestroy() {
+    this.events.events.forEach((todo) => {
+      if (todo.timerSubscription) {
+        todo.timerSubscription.unsubscribe();
+      }
+    });
+  }
+
   updateCurrEvents(): void {
     if (this.selected) {
-      this.currEvents = this.events.events.filter(event =>
-        event.date.toDateString() === this.selected?.toDateString()
+      this.currEvents = this.events.events.filter(
+        (event) => event.date.toDateString() === this.selected?.toDateString()
       );
     }
   }
+
   truncate(text: string, limit: number): string {
     return text.length > limit ? text.substring(0, limit) + '...' : text;
   }
+
   onSubmit(): void {
     if (this.title.value && this.desc.value && this.selected) {
-      this.events.events.push({
+      console.log(this.color);
+      
+      const event: Event = {
         id: this.events.index++,
         title: this.title.value,
         desc: this.desc.value,
-        date: this.selected
-      });
+        date: this.selected,
+        color: this.color,
+      };
+      this.events.events.push(event);
       this.title.reset();
       this.desc.reset();
       this.updateCurrEvents();
-    }
-    else{
-      alert('pls fill out all details')
+
+      if (
+        this.events.timeDifferenceInSeconds(
+          this.events.todaysDate.toTimeString().substring(0, 5),
+          '12:00'
+        ) && this.events.todaysDate.getDay() === this.selected.getDay()
+      ) {
+        this.events.startTaskTimer(event);
+      }
+    } else {
+      alert('Please fill out all details');
     }
   }
 }
